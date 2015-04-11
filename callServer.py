f48628f8cfdddf7be2929399b7e93e6a13ce2c57
revanth <revanth.reddy@gmail.com>
2015-04-10 23:54:17 -0500

    renamed the server name

--- callServer.py
+++ callServer.py
@@ -1,7 +1,7 @@
 import requests
 
 def BoomerIsFed():
-	return requests.post("http://ec2-52-11-35-31.us-west-2.compute.amazonaws.com:3002/send")
+	return requests.post("http://ec2-54-149-172-249.us-west-2.compute.amazonaws.com:3002/send")
 
 
 if __name__=='__main__':
